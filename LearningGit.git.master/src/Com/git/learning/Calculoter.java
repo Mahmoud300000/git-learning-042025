284618bf87be2297d276a5603e1e63c65275c10f
mmand <mmand@MAHMOUD>
2025-04-12 08:23:27 -0400

    fix a code

--- LearningGit.git.master/src/Com/git/learning/Calculoter.java
+++ LearningGit.git.master/src/Com/git/learning/Calculoter.java
@@ -1,19 +1,42 @@
 package Com.git.learning;
 
+
+
+
 public class Calculoter {
 
 	public int add (int x , int y) {
-		return x + y;
+		int result =0;
+		result = x+y;
+		return result;
+		
 	}
+	public int multiply(int x , int y) {
+		return x *y;	
+		}
+	
 	
 	public int divid(int x , int y) {
-		
-		int result =0 
-		if (y ! = 0) {
-			result = x / y ;
-		}else{
-			throw new Exception(" you cannot divide number by Zero. ")
-		}
-	}
+	    int result = 0; 
+		try {
+	    if(y!=0) {
+	     result  = x / y ;
+	    		
+	    		
+	    		}else{
+	    			throw new Exception(" you cannot divide number by Zero. ");
+	    		}
+	    		    
+	    	}catch (Exception e) {
+	    		e.printStackTrace();
+	    	}
+	    	return result;
+	    }	
+	    	
+        }	
+	    	
+	    	
+	    	
+	    
 	
-}
+	
