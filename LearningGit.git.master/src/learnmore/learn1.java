6c849759e99637791ef93265c72108f43fa315f2
mmand <mmand@MAHMOUD>
2026-02-24 16:37:56 +0200

    new

--- LearningGit.git.master/src/learnmore/learn1.java
+++ LearningGit.git.master/src/learnmore/learn1.java
@@ -1,5 +1,16 @@
 package learnmore;
 
 public class learn1 {
-
+	 String name;
+     String color;
+     String model;
+     String year;
+     int mileage;
+     
+     public void displayCarInfo() {
+         System.out.println(name + " " + color + " " + model + " " + year);
+     }
+     public void start() {
+         System.out.println("Car is starting ....");
+     }
 }
